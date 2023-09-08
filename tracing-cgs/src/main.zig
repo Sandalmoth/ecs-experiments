30e2a2e9f8a90bfaeac847e11b9d852e35a5b7b5
Jonathan <sandalmoth@pm.me>
2023-09-09 00:30:31 +0200

    add iterator

--- tracing-cgs/src/main.zig
+++ tracing-cgs/src/main.zig
@@ -32,6 +32,32 @@ pub fn MemoryPool(comptime Item: type) type {
             active: [page_size]bool,
         };
 
+        pub const Iterator = struct {
+            page: ?*Page,
+            cursor: usize,
+
+            pub fn next(iter: *Iterator) ?*Item {
+                if (iter.page == null) {
+                    return null;
+                }
+
+                while (iter.cursor < page_size and
+                    !iter.page.?.active[iter.cursor]) : (iter.cursor += 1)
+                {}
+
+                if (iter.cursor == page_size) {
+                    iter.page = iter.page.?.next;
+                    iter.cursor = 0;
+                    return iter.next(); // consider forcing tail-call optimization
+                }
+
+                std.debug.assert(iter.page.?.active[iter.cursor]);
+                const node = &iter.page.?.items[iter.cursor];
+                iter.cursor += 1;
+                return @ptrCast(node);
+            }
+        };
+
         alloc: std.mem.Allocator,
 
         free_list: ?*Node = null,
@@ -115,6 +141,13 @@ pub fn MemoryPool(comptime Item: type) type {
             node.* = Node{ .next = pool.free_list };
             pool.free_list = node;
         }
+
+        pub fn iterator(pool: *Self) Iterator {
+            return Iterator{
+                .page = pool.page_list,
+                .cursor = 0,
+            };
+        }
     };
 }
 
@@ -159,6 +192,47 @@ test "MemoryPool create & destroy" {
     // the slots should have been reused earlier on
     try std.testing.expect(c != try pool.create());
     try std.testing.expect(a != try pool.create());
+
+    // finally, randomly create and destroy stuff, to discover potential leaks or something
+    var fuzz = MemoryPool(@Vector(4, f32)).init(std.testing.allocator);
+    defer fuzz.deinit();
+    var rng = std.rand.DefaultPrng.init(2701);
+    for (0..123456) |_| {
+        var olditem: ?*@Vector(4, f32) = null;
+        if (fuzz.len == 0 or olditem == null or rng.random().float(f32) < 0.66) {
+            const x = try fuzz.create();
+            if (olditem == null or rng.random().float(f32) < 0.33) {
+                olditem = x;
+            }
+        } else {
+            fuzz.destroy(olditem.?);
+        }
+    }
+}
+
+test "MemoryPool iter" {
+    var pool = MemoryPool(i32).init(std.testing.allocator);
+    defer pool.deinit();
+
+    const N = @TypeOf(pool).page_size + 1;
+
+    for (0..N) |_| {
+        _ = try pool.create();
+    }
+
+    var i: i32 = 0;
+    var iter = pool.iterator();
+    while (iter.next()) |x| {
+        x.* = i * i;
+        i += 1;
+    }
+
+    i = 0;
+    iter = pool.iterator();
+    while (iter.next()) |x| {
+        try std.testing.expectEqual(i * i, x.*);
+        i += 1;
+    }
 }
 
 pub const State = struct {
