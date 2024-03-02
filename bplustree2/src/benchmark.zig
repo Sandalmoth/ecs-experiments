3371433411703176088e4adef1a294a146f0e29c
Jonathan <sandalmoth@pm.me>
2024-03-02 02:04:32 +0100

    add benchmark, and it's dissapointingly slow

--- bplustree2/src/benchmark.zig
+++ bplustree2/src/benchmark.zig
@@ -6,5 +6,44 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
-    _ = alloc;
+    const M = 21;
+
+    std.debug.print("len\tcrt_ns\tget_ns\n", .{});
+
+    var acc: @Vector(4, f32) = @splat(0.0);
+
+    for (0..M) |m| {
+        const len = @as(usize, 1) << @intCast(m);
+        var i: u32 = 2_654_435_761;
+        var s = Storage(u32, @Vector(4, f32), 31, 15).init(alloc);
+        i +%= 2_654_435_761;
+        defer s.deinit();
+
+        var timer = try std.time.Timer.start();
+
+        for (0..len) |j| {
+            try s.add(i, @as(@Vector(4, f32), @splat(@floatFromInt(j))));
+            i +%= 2_654_435_761;
+        }
+
+        const create_time: f64 = @floatFromInt(timer.lap());
+
+        i = 2_654_435_761;
+        i +%= 2_654_435_761;
+        for (0..len) |_| {
+            acc += s.get(i).?.*;
+            i +%= 2_654_435_761;
+        }
+
+        const get_time: f64 = @floatFromInt(timer.lap());
+
+        const il2: f32 = 1 / @as(f32, @floatFromInt(len));
+
+        std.debug.print(
+            "{}\t{d:.2}\t{d:.2}\n",
+            .{ len, create_time * il2, get_time * il2 },
+        );
+    }
+
+    std.debug.print("printing to avoid optimizer dropping my result\n{}\n", .{acc});
 }
