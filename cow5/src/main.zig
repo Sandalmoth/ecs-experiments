14db369fc864af2ec4468c29cbe4f3320266b665
Jonathan <sandalmoth@pm.me>
2024-03-27 10:19:20 +0100

    add ability to extract sorted values from hashmap

--- cow5/src/main.zig
+++ cow5/src/main.zig
@@ -22,7 +22,7 @@ const MapHeader = struct {
     keys: [*]Entity,
     vals: *anyopaque,
     meta: [*]Meta,
-    len: usize,
+    len: i32, // if we have more deletes than creates, this is negative
     capacity: usize,
     mod: *const fn (usize) usize, // x -> x % capacity
 };
@@ -132,6 +132,67 @@ const Map = struct {
         map.head.len += 1;
     }
 
+    /// NOTE this destroys the internal structure, map can not be used after this is called
+    fn sorted(map: *Map, comptime V: type) SortedIterator(V) {
+        const keys = map.head.keys;
+        const vals: [*]V = @alignCast(@ptrCast(map.head.vals));
+
+        // compact values at front of hashmap
+        var i: usize = 0;
+        for (0..map.head.capacity) |j| {
+            if (keys[j] != nil) {
+                keys[i] = keys[j];
+                vals[i] = vals[j];
+                i += 1;
+            }
+        }
+
+        // then sort them
+        var unsorted = true;
+        while (unsorted) {
+            unsorted = false;
+            for (1..i) |j| {
+                if (keys[j - 1] > keys[j]) {
+                    const tmpkey = keys[j];
+                    keys[j] = keys[j - 1];
+                    keys[j - 1] = tmpkey;
+                    const tmpval = vals[j];
+                    vals[j] = vals[j - 1];
+                    vals[j - 1] = tmpval;
+                    unsorted = true;
+                }
+            }
+        }
+
+        return .{
+            .keys = keys,
+            .vals = vals,
+            .capacity = i,
+            .cursor = 0,
+        };
+    }
+
+    fn SortedIterator(comptime V: type) type {
+        return struct {
+            const Entry = struct { key: Entity, val_ptr: *V };
+
+            keys: [*]Entity,
+            vals: [*]V,
+            capacity: usize,
+            cursor: usize,
+
+            fn next(it: *@This()) ?Entry {
+                if (it.cursor >= it.capacity) return null;
+                const result = Entry{
+                    .key = it.keys[it.cursor],
+                    .val_ptr = &it.vals[it.cursor],
+                };
+                it.cursor += 1;
+                return result;
+            }
+        };
+    }
+
     fn hash(key: Entity) u32 {
         return std.hash.XxHash32.hash(2654435761, std.mem.asBytes(&key));
     }
@@ -158,7 +219,7 @@ const Run = struct {
         run.head.keys = @ptrFromInt(data + @offsetOf(Layout, "keys"));
         run.head.vals = @ptrFromInt(data + @offsetOf(Layout, "vals"));
         run.head.len = 0;
-        run.head.capacity = 0;
+        run.head.capacity = capacity;
         run.head.map = null;
         run.head.rc = 0;
 
@@ -237,6 +298,8 @@ const Storage = struct {
     }
 
     pub fn add(storage: *Storage, comptime V: type, key: Entity, val: V) !void {
+        if (storage.immutable) return error.Immutable;
+
         const index = storage.index orelse {
             storage.index = @ptrCast(@alignCast(try storage.alloc.create(Page)));
             errdefer storage.alloc.destroy(@as(*Page, @ptrCast(@alignCast(storage.index))));
@@ -252,6 +315,46 @@ const Storage = struct {
         var i: usize = 0;
         while (key < index.bounds[i]) : (i += 1) {}
         try index.buckets[i].?.add(V, storage.alloc, key, val);
+    }
+
+    pub fn compact(storage: *Storage, comptime V: type) !void {
+        storage.immutable = true; // TODO move to replicate
+
+        const index = storage.index orelse return;
+        std.debug.assert(storage.n_runs > 0);
+        var i = storage.n_runs;
+        while (i > 0) {
+            i -= 1;
+            const run = index.buckets[i].?;
+            const map = run.head.map orelse continue;
+
+            // how many runs will we need for this run + map after compacting?
+            const len = @as(i32, @intCast(run.head.len)) + map.head.len;
+            if (len <= 0) {
+                // I think shuffle everything to the right in the index and destroy this one?
+                @panic("TODO");
+            }
+            const n_pages = @as(usize, @intCast(len)) / run.head.capacity + 1;
+            std.mem.copyBackwards(
+                ?*Run,
+                index.buckets[i + n_pages ..],
+                index.buckets[i .. i + n_pages],
+            );
+            for (0..n_pages) |j| {
+                // handle failure?
+                index.buckets[i + j] = Run.create(V, storage.alloc) catch
+                    @panic("Allocation failure in Storage.compact");
+            }
+
+            // we have our run and map, as well as enough new buckets to store merged result
+            // now, fill all the buckets evenly with values from the run & map
+            var it_map = map.sorted(V);
+            while (it_map.next()) |kv_map| {
+                std.debug.print("{}\n", .{kv_map.key});
+            }
+
+            std.debug.print("{} {} {}\n", .{ i, len, n_pages });
+        }
     }
 
     fn debugPrint(storage: *Storage) void {
@@ -281,4 +384,16 @@ pub fn main() !void {
         try s.add(u32, i, @intCast(i));
         s.debugPrint();
     }
+
+    try s.compact(u32);
+    s.debugPrint();
+
+    for (10..20) |i| {
+        std.debug.print("inserting {}\n", .{i});
+        try s.add(u32, i, @intCast(i));
+        s.debugPrint();
+    }
+
+    try s.compact(u32);
+    s.debugPrint();
 }
